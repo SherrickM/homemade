7beaeb559d45e4ab3867cb2130c55d8a58ed9816
Trevor Hilimire <trevorhilimire@gmail.com>
2022-04-11 21:22:51 -0400

    adds api for sponacular to js file

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=apples%2Cflour%2Csugar&number=5&ignorePantry=true&ranking=1",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+		"X-RapidAPI-Key": "c4da594f4amsh53139c876b46e00p155ca6jsn0a6b630dd26b"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
