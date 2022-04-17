307774fe1d5223fc84b10537ef82cc0c5333711d
Trevor Hilimire <trevorhilimire@gmail.com>
2022-04-17 08:38:48 -0400

    builds functionality for toggle button, sets up local storage for drinks list, adds delete button functionality

--- assets/scriptTest.js
+++ assets/scriptTest.js
@@ -1,64 +1,190 @@
-// creates array to hold user inputs of ingredients
+// creates event listener and code for click on toggle button between food and drink
+$("#toggleBtn").click(toggleFoodDrink);
+// functionality of toggle botton between food and drink
+function toggleFoodDrink(event){
+    // togogles the class that moves the inner circle to the left and right"
+    $("#toggleCircle").toggleClass("translate-x-7");
+    // toggle background of food class
+    $("#food").toggleClass("bgMidL");
+    $("#drink").toggleClass("bgMidL");
+    // if the data-v value is "food", then change it to "drink"
+    if($("#addIngredientsButton").data("v") == "food"){
+        $("#addIngredientsButton").data("v", "drink");
+        console.log($("#addIngredientsButton").data("v"));
+    }
+    // if the data-v value is "drink", then change it to "food"
+    else{
+        // $("#drink").toggleClass("bgMidL");
+        $("#addIngredientsButton").data("v", "food");
+        console.log($("#addIngredientsButton").data("v"))
+    }
+    
+    renderIngredientsList();
+
+};
+
+// variable for stored drink ingredintes
+var drinkIngredents = [];
+
+// variable for stored food ingredients
 var ingredientsList = [];
-// intilizises local storage and populates ingredents list var if not empty
+
+
+// // intilizises local storage and populates ingredents list var if not empty
 function init() {
     var storedIngredientsList = JSON.parse(localStorage.getItem("storedIngredients"));
-    
+    // deletes last food item if list is > 5
+    if (ingredientsList > 5){
+        ingredientsList.pop()
+    };
+    // if local storage contains items, bring them into food ingredients list
     if (storedIngredientsList !== null) {
         ingredientsList = storedIngredientsList;
     };
       
-
+    // adds food ingredients list to stored food ingredients in local storage(storedIngredients)
     localStorage.setItem("storedIngredients", JSON.stringify(ingredientsList));
 
-};
+    // init for drink list
+    var storedDrinkIngredientsList = JSON.parse(localStorage.getItem("storedDrinkIngredients"));
 
+    // if local storage contains items, bring them into drink ingredients list
+    if (storedDrinkIngredientsList !== null) {
+        drinkIngredents = storedDrinkIngredientsList;
+    };
+      
+    // adds food ingredients list to stored drink ingredients in local storage(storedDrinkIngredients)
+    localStorage.setItem("storedDrinkIngredients", JSON.stringify(drinkIngredents));
+
+};
+// calls init function to initilize local storage
 init();
 
 // stores ingredients to local stroage
 function storeIngredients(){
+    // sets food ingredients list into local storage
     localStorage.setItem("storedIngredients", JSON.stringify(ingredientsList));
+
+    // store drinks list
+    localStorage.setItem("storedDrinkIngredients", JSON.stringify(drinkIngredents));
 
 }
 
 $("#addIngredientsButton").click(function(){
-    if(userInput !== null){
-        renderIngredientsList();
-        var userInput = $("#userInputIngredientText").val();
-        ingredientsList.push(userInput);
-        storeIngredients();
-
+    // limits length of ingredients of drinks then food to no more than 5
+    if (drinkIngredents.length > 4){
+        drinkIngredents.pop()
     };
+    if (ingredientsList.length > 4){
+        ingredientsList.pop()
+    };
+    // if data attribute "v" of addIngredientsButton is food, add user input in userInputIngredientText
+    // to the (food) ingredientsList. render to page and save to local storage
+    if($("#addIngredientsButton").data("v") == "food"){
+        var userInput = $("#userInputIngredientText").val();
+        if(userInput !== null){
+            ingredientsList.splice(0,0, userInput);
+            renderIngredientsList();
+            storeIngredients();
+        };
+    // if data attribute "v" of addIngredientsButton is drink, add user input in userInputIngredientText
+    // to the (drink) drinkIngredents list. render to page and save to local storage
+    } else if ($("#addIngredientsButton").data("v") == "drink") {
+        var userInput = $("#userInputIngredientText").val();
+        if(userInput !== null){
+            drinkIngredents.splice(0,0, userInput);
+            renderIngredientsList();
+            storeIngredients();
+        };
+
+    }
+    // clears the user input after being added
+    $("#userInputIngredientText").val("");
+
+    
+    
 });
 
 function renderIngredientsList(){
-    // var userInput = $("#userInputIngredientText").val()
+    // clears rendered list ingredents from page to prevent double rendering
     $("#ingredientsListSaved").empty();
-    ingredientsList.forEach((value, key) => {
+    // creates variable for current list for alternating between food and drink
+    var currentList = null;
+    // if toogle switch's data-v = food, populate currentList with food ingredients, otherwise
+    // populate with drink list
+    if($("#addIngredientsButton").data("v") == "food"){
+        currentList = ingredientsList;
+    } else {
+        currentList = drinkIngredents
+        };
+    // for each value in the currentList, render html from list
+    currentList.forEach((value, key) => {
         var index = key;
         var userInput = value;
         var listHtml = 
-        $(`<li id="listbox-item-`+index+`" role="option" class="text-gray-900 cursor-default hover:bg-indigo-500 hover:text-white select-none relative py-2 pl-3 pr-9">
-            <div class="flex items-center">
-                <span class="ml-3 block font-normal truncate">
-                    `+userInput+`
+        $(`<li id="listbox-item-`+index+`" role="option"
+        class="text-gray-900 h-16 cursor-default hover:text-gray-500 select-none relative py-2 pl-3 pr-9">
+        <div class="grid grid-cols-12 h-16">
+            <div class="flex items-center col-span-10 h-auto">
+                <span class="ml-3 block truncate text-lg font-bold font-gray-800">
+                `+userInput+`
                 </span>
             </div>
-            <span class="absolute inset-y-0 right-0 flex items-center pr-4">
-                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd">
+            <span class=" col-end-13 col-span-1 h-12 w-12 flex justify-center my-auto pr-4">
+                <svg class="deleteBtn cursor-pointer stroke-2 h-30 w-30  text-red-500 hover:text-red-700" data-i=`+index+`
+                    fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z">
                     </path>
                 </svg>
             </span>
-        </li>`);
-        console.log(value, key)
-        
+                    </li>`);
 
-        $("#ingredientsListSaved").append(listHtml);
+                    $("#ingredientsListSaved").append(listHtml);
+     
+     });
+                
+};
 
-        
-    });
+// calls function to render lists from local storage to page
+renderIngredientsList();
+
+
+// adds funtionality to delete button
+// on click of delete button, run function deleteListItem
+$("#ingredientsListSaved").on("click", ".deleteBtn", deleteListItem);
+// function for deleting list items
+function deleteListItem (){
+    // selects the index value of the list item (data(i) property in html) for deletion
+    var deleteBtnIndex = $(this).data("i");
+    // if toggled to food, delete from food list, otherwise delete from drink
+    if($("#addIngredientsButton").data("v") == "food"){
+        ingredientsList.splice(deleteBtnIndex, 1);
+        console.log(ingredientsList);
+    } else {
+        drinkIngredents.splice(deleteBtnIndex, 1);
+        };
+    // render and store new lists
+    renderIngredientsList();
+    storeIngredients();
 
 };
 
-renderIngredientsList();
+// Adds functionality to carousel 2 that displays search results
+
+let defaultTransform = 0;
+function goNext() {
+    defaultTransform = defaultTransform - 398;
+    var slider = document.getElementById("slider");
+    if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7) defaultTransform = 0;
+    slider.style.transform = "translateX(" + defaultTransform + "px)";
+}
+next.addEventListener("click", goNext);
+function goPrev() {
+    var slider = document.getElementById("slider");
+    if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
+    else defaultTransform = defaultTransform + 398;
+    slider.style.transform = "translateX(" + defaultTransform + "px)";
+}
+prev.addEventListener("click", goPrev);
